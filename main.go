f7404c481d5a71472f12b16b86506e878a8aab22
Tadeu <tadeu.tupiz@gmail.com>
2021-06-26 22:33:10 -0300

    feat: initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"jwt-api-gin/controllers"
+	"jwt-api-gin/middlewares"
+	"jwt-api-gin/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	models.ConnectDataBase()
+	r := gin.Default()
+	public := r.Group("/api")
+	public.POST("/register", controllers.Register)
+	public.POST("/login", controllers.Login)
+
+	protected := r.Group("/api/admin")
+	protected.Use(middlewares.JwtAuthMiddleware())
+	protected.GET("/user", controllers.CurrentUser)
+
+	r.Run(":8080")
+}
